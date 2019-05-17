4b0230a8360f43acc019bcbb468b7e528ca482f5
gopesh-sharma <gopesh_sharma@gensler.com>
2019-05-17 16:26:14 +0530

    Removed natural and botkit package

--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
-const NLP = require('natural');
 const fs = require('fs');
 var csv = require('csv-parser'); 
 const Discordie = require('discordie');
 const Events = Discordie.Events;
 const client = new Discordie();
-const Analyzer = require('natural').SentimentAnalyzer;
-const stemmer = require('natural').PorterStemmer;
-const analyzer = new Analyzer("English", stemmer, "afinn");
 const { NlpManager } = require('node-nlp');
 const manager = new NlpManager({ languages: ['en'] });
-const trainnlp = require('./train-nlp');
+const trainnlp = require('./data/train-nlp');
 const threshold = 0.5;
+const modelPath = 'model/model.nlp';
+const trainedDataPath = 'data/qatraining1.csv';
+const unmatchedFile = 'unmatched/data.txt';
 
 require('dotenv').config();
 
-const classifier = new NLP.LogisticRegressionClassifier();
 const token = process.env.DISCORD_API_TOKEN;
 
 client.connect({
     token: token
 });
 client.Dispatcher.on(Events.GATEWAY_READY, async e => {
-    var trainedDataPath = 'qatraining1.csv';
-    var jsonData = await parseCSV(trainedDataPath);
+    let jsonData = await parseCSV(trainedDataPath);
     trainData(jsonData);
     console.log('Connected as: ' + client.User.username);
 });
@@ -51,18 +48,18 @@ async function parseCSV(path) {
 }
 
 function trainData(jsonData) {
-    fs.access('model.nlp', fs.constants.F_OK, (err) => {
+    fs.access(modelPath, fs.constants.F_OK, (err) => {
         if(err) {
             console.log(err);
-            train(jsonData, classifier)
+            train(jsonData)
         } else {
             console.log('trained...');
-            manager.load('model.nlp');
+            manager.load(modelPath);
         }
     });
 }
 
-async function train(jsonData, classifier) {
+async function train(jsonData) {
     jsonData.forEach(function(obj) { 
         manager.addDocument('en', obj.question.toLowerCase(), obj.intent + obj.counter);
         manager.addAnswer('en', obj.intent + obj.counter, obj.answer.toLowerCase());
@@ -73,12 +70,14 @@ async function train(jsonData, classifier) {
     await manager.train();
     const hrend = process.hrtime(hrstart);
     console.info('Trained (hr): %ds %dms', hrend[0], hrend[1] / 1000000);
-    manager.save('model.nlp');
+    manager.save(modelPath);
 }
 
 async function handleMessage(e, message) {
   const response = await manager.process('en', message); 
-  const answer = response.score > threshold && result.answer ? result.answer: "Sorry, I don't know what do you mean";
-  
-  e.message.channel.sendMessage(response.answer);
+  const answer = response.score > threshold && response.answer ? response.answer: "Sorry, I don't know what do you mean";
+  console.log(response);
+  const unmatcheResponse = response.score < threshold ? `Message : ${message} - response : ${answer}\n` : '';
+  fs.appendFile(unmatchedFile, unmatcheResponse, function () { });
+  e.message.channel.sendMessage(answer);
 }
